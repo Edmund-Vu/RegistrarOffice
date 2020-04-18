3f7388f2f0aaba506f3c5c9f53975f041996b486
Edmund <edmundvu816@gmail.com>
2020-04-18 08:03:59 +0000

    Added header file for student template.

---
+++ Student.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Student{
+  public:
+    Students();
+    Students(int t, int e);
+    ~Students();
+
+    int windowTime;
+    int entryTime;
+    int queueTime;
+    int waitTime;
+}
