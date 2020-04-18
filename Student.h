319dbd142dcf80957c56dff17512499c0e152627
Edmund <edmundvu816@gmail.com>
2020-04-18 08:12:42 +0000

    converted into a template class.

--- Student.h
+++ Student.h
@@ -2,14 +2,36 @@
 
 using namespace std;
 
+template <class T>
 class Student{
   public:
-    Students();
-    Students(int t, int e);
-    ~Students();
-
     int windowTime;
     int entryTime;
     int queueTime;
-    int waitTime;
+    int idleTime;
+
+    Student();
+    Student(int t, int e);
+    ~Student();
+};
+
+template <class T>
+Student<T>::Student(){
+  windowTime = 0;
+  entryTime = 0;
+  queueTime = 0;
+  idleTime = 0;
+}
+
+template <class T>
+Student<T>::Student(int t, int e){
+  windowTime = t;
+  entryTime = e;
+  queueTime = 0;
+  idleTime = 0;
+}
+
+template <class T>
+Student<T>::~Student(){
+
 }
