324fd47eecffd3040a4c5d86c4228fb7404b58fc
Edmund <edmundvu816@gmail.com>
2020-04-22 20:38:09 +0000

    New file to hold info for Student header file.

---
+++ Student.cpp
@@ -0,0 +1,21 @@
+#include "Student.h"
+
+using namespace std;
+
+Student::Student(){
+  windowTime = 0;
+  entryTIme = 0;
+  queueTime = 0;
+  idleTime = 0;
+}
+
+Student::Student(int t, int e){
+  windowTime = t;
+  entryTime = e;
+  queueTime = 0;
+  idleTime = 0;
+}
+
+Student::~Student(){
+  
+}
